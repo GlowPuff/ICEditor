7b0b2b911c40b83235da051ae44ada16cd16a3c8
GlowPuff <glowpuffmobile@gmail.com>
2022-06-07 14:48:13 -0400

    added missing Bespin/Lothal tiles, startup window is wider

--- Common/Utils.cs
+++ Common/Utils.cs
@@ -13,7 +13,7 @@ namespace Imperial_Commander_Editor
 	public static class Utils
 	{
 		public const string formatVersion = "14";
-		public const string appVersion = "1.0.10";
+		public const string appVersion = "1.0.11";
 
 		public static List<DeploymentCard> allyData;
 		public static List<DeploymentCard> enemyData;

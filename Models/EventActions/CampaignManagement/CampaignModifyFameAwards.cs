a2b6853461ae89c5eacc621f91dffea47aa867ab
GlowPuff <glowpuffmobile@gmail.com>
2024-03-03 22:09:13 -0500

    remove customIdentifier from CampaignModifyFameAwards

--- Models/EventActions/CampaignManagement/CampaignModifyFameAwards.cs
+++ Models/EventActions/CampaignManagement/CampaignModifyFameAwards.cs
@@ -3,10 +3,9 @@
 	public class CampaignModifyFameAwards : EventAction
 	{
 		int _fameToAdd, _awardsToAdd;
-		string _customIdentifier;
+
 		public int fameToAdd { get => _fameToAdd; set { _fameToAdd = value; PC(); } }
 		public int awardsToAdd { get => _awardsToAdd; set { _awardsToAdd = value; PC(); } }
-		public string customIdentifier { get => _customIdentifier; set { _customIdentifier = value; PC(); } }
 
 		public CampaignModifyFameAwards()
 		{
@@ -16,7 +15,6 @@
 		public CampaignModifyFameAwards( string dname, EventActionType et ) : base( et, dname )
 		{
 			fameToAdd = awardsToAdd = 0;
-			customIdentifier = "";
 		}
 	}
 }
